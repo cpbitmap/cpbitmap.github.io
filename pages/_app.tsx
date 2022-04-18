c5a6288556b96c390b698f10a02ae508457e2cc8
Ainsley RUtterford <ainsley.rutterford@gmail.com>
2022-04-18 01:22:06 +0100

    Add static css generation, add favicons, update about page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import "../styles/global.css";
 
-import { ThemeProvider } from "@primer/react";
+import { SSRProvider, ThemeProvider } from "@primer/react";
 import type { AppProps } from "next/app";
 import React from "react";
 
@@ -9,13 +9,15 @@ import Layout from "../components/Layout";
 import githubStyles from "../styles/github.module.css";
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <ThemeProvider>
-    <div className={githubStyles["markdown-body"]}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </div>
-  </ThemeProvider>
+  <SSRProvider>
+    <ThemeProvider>
+      <div className={githubStyles["markdown-body"]}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </div>
+    </ThemeProvider>
+  </SSRProvider>
 );
 
 export default MyApp;
--- pages/about.tsx
+++ pages/about.tsx
@@ -7,12 +7,15 @@ import WidthContainer from "../components/WidthContainer";
 const About: NextPage = () => {
   const { theme } = useTheme();
   return (
-    <WidthContainer style={{ paddingBottom: 64 }}>
+    <WidthContainer style={{ paddingBottom: "64px !important" }}>
       <Heading
         as="h1"
-        style={{ textAlign: "center" }}
+        // style={{ textAlign: "center" }}
         sx={{
           pb: 1,
+          borderBottomWidth: 1,
+          borderBottomColor: "border.default",
+          borderBottomStyle: "solid",
         }}
       >
         <Link
@@ -42,7 +45,32 @@ const About: NextPage = () => {
       </Heading>
       <Text as="p">
         The cpbitmap file format is a proprietary format created by Apple to
-        store iOS background images.
+        store iOS background images. The most common examples are the{" "}
+        <code>LockBackground.cpbitmap</code> and{" "}
+        <code>HomeBackground.cpbitmap</code> files found in iPhone or iPad
+        backups.
+      </Text>
+      <Heading
+        as="h2"
+        sx={{
+          fontSize: 3,
+        }}
+      >
+        My image isn&apos;t being converted properly
+      </Heading>
+      <Text as="p">
+        If the output image is corrupt or looks like it has been stretched and
+        distorted, try selecting another iOS version and exporting again. The
+        cpbitmap format has changed slightly over the years and may change again
+        at some point in the future. If this tool is no longer working with a
+        new version of iOS, consider opening an issue on{" "}
+        <Link
+          href="https://github.com/cpbitmap/cpbitmap.github.io"
+          style={{ color: theme?.colorSchemes.light.colors.btn.outline.text }}
+        >
+          GitHub
+        </Link>
+        .
       </Text>
       <Heading
         as="h2"
@@ -57,7 +85,7 @@ const About: NextPage = () => {
         How do we know how to convert cpbitmap files?
       </Heading>
       <Text as="p">
-        Apple has never released any documentation about the layout of cpbitmap
+        Apple have never released any documentation about the layout of cpbitmap
         files. We only know how to convert cpbitmap images due to some clever
         guessing and trial and error by{" "}
         <Link
@@ -70,16 +98,16 @@ const About: NextPage = () => {
       </Text>
       <Text as="p">
         There are a few scripts that use his description to convert cpbitmap
-        images to other formats, but I couldn&apos;t find a working online
-        version. The code used in this tool was originally written by{" "}
+        images to other formats, but there doesn&apos;t seem to be a working
+        browser based solution. The code used in this tool was originally
+        written by{" "}
         <Link
           href="https://stackoverflow.com/a/48158807"
           style={{ color: theme?.colorSchemes.light.colors.btn.outline.text }}
         >
           dr15 in this Stack Overflow answer
         </Link>{" "}
-        to work with Node.js, but has been slightly edited to run in the
-        browser.
+        to run in Node.js, so it has been slightly edited to run in the browser.
       </Text>
     </WidthContainer>
   );
