44d34a9cce170093bd42156bbe6ff49c74745427
Ainsley Rutterford <ainsley.rutterford@gmail.com>
2022-04-23 21:20:27 +0100

    Form labels are now static HTML

--- components/Select/index.tsx
+++ components/Select/index.tsx
@@ -1,25 +1,45 @@
-import { FormControl, Select as PrimerSelect } from "@primer/react";
+import { FormControl, Select as PrimerSelect, Text } from "@primer/react";
 import React from "react";
 
 type SelectProps = {
   options: string[];
+  label: string;
   value?: string;
   onChange?: React.ChangeEventHandler<HTMLSelectElement>;
-  label?: string;
   style?: React.CSSProperties;
 };
 
 const Select = ({ options, value, label, onChange, style }: SelectProps) => (
-  <FormControl id={label} sx={{ ...style, width: "100%" }}>
-    {label !== undefined && <FormControl.Label>{label}</FormControl.Label>}
-    <PrimerSelect onChange={onChange} value={value}>
-      {options.map((option) => (
-        <PrimerSelect.Option key={option} value={option}>
-          {option}
-        </PrimerSelect.Option>
-      ))}
-    </PrimerSelect>
-  </FormControl>
+  <div id={label} style={{ ...style, width: "100%" }}>
+    <label htmlFor={label} style={{ width: "100%" }}>
+      <Text
+        as="p"
+        sx={{
+          fontSize: 1,
+          fontWeight: "bold",
+          m: 0,
+          p: 0,
+          pb: 1,
+        }}
+      >
+        {label}
+      </Text>
+    </label>
+    <FormControl sx={{ width: "100%" }}>
+      <PrimerSelect
+        id={label}
+        onChange={onChange}
+        value={value}
+        sx={{ width: "100%" }}
+      >
+        {options.map((option) => (
+          <PrimerSelect.Option key={option} value={option}>
+            {option}
+          </PrimerSelect.Option>
+        ))}
+      </PrimerSelect>
+    </FormControl>
+  </div>
 );
 
 export default Select;
